b66e170998bb708f8662a18a29eb9857c4370f6b
freekatz <katz@mtxai.io>
2025-01-10 13:26:46 +0800

    reize ffhq and split ffhq

--- scripts/split_ffhq_dataset.py
+++ scripts/split_ffhq_dataset.py
@@ -5,8 +5,8 @@ import argparse
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--out', type=str)
-parser.add_argument('--num', default=500, type=int)
+parser.add_argument('-o', type=str)
+parser.add_argument('-n', default=500, type=int, help='number of val size')
 parser.add_argument('--sort', action='store_true')
 parser.add_argument('--seed', default=np.random.randint(0, 10000), type=int)
 args = parser.parse_args()
@@ -14,13 +14,13 @@ args = parser.parse_args()
 pic_ids = list(range(70000))
 random.seed(args.seed)
 random.shuffle(pic_ids)
-num_train = int(len(pic_ids) - args.num)
+num_train = int(len(pic_ids) - args.n)
 
 train_ids = pic_ids[:num_train]
 if args.sort:
     train_ids = sorted(train_ids, reverse=False)
 
-train_ids_file = os.path.join(args.out, 'ffhq_train.txt')
+train_ids_file = os.path.join(args.o, 'ffhq_train.txt')
 with open(train_ids_file, mode='w') as ff:
     for pic_id in train_ids:
         ff.write(f'{pic_id:05d}.png'+'\n')
@@ -30,7 +30,7 @@ print(f'Train set: {num_train}/{len(pic_ids)}, will write to {train_ids_file}')
 val_ids = pic_ids[num_train:]
 if args.sort:
     val_ids = sorted(val_ids, reverse=False)
-val_ids_file = os.path.join(args.out, 'ffhq_val.txt')
+val_ids_file = os.path.join(args.o, 'ffhq_val.txt')
 with open(val_ids_file, mode='w') as ff:
     for pic_id in val_ids:
         ff.write(f'{pic_id:05d}.png'+'\n')
